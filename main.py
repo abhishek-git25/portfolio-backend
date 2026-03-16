1211bf25b540d39d9ba61b6a0e7c5fa7506b0874
abhishekpgs <abhishek@pgsuae.com>
2026-03-16 23:40:41 +0530

    allow all the origins

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = create_app()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
